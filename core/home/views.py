056aa11681d5c6bb80f48f73e7abe0845f1cd521
vanshii778 <vanshikathesiya@fxis.ai>
2025-09-01 10:53:20 +0530

    Template Engine

--- core/home/views.py
+++ core/home/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def home(request):
+    peoples = [
+        {'name':'Vanshika','age':26},
+        {'name':'cutiee','age':12}
+    ]
+    
+    return render(request,"home/index.html", context={'page':'Django Learning','peoples':peoples})
+
+def about(request):
+    context = {'page':'About'}
+    return render(request,"home/about.html",context)
+
+def contact(request):
+    context = {'page':'Contact'}
+
+    return render(request,"home/contact.html",context)
+
+def success_page(request):
+    return HttpResponse("<h1>Hey This is a success page.<h1>")
